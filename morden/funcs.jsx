00cf9da3d819b92340a43d6d819a96a830a9750c
김지환 <kimjihwan@gimjihwan-ui-MacBookPro.local>
2023-11-20 17:05:20 +0900

    add constructor functions

--- morden/funcs.jsx
+++ morden/funcs.jsx
@@ -20,3 +20,17 @@ var circle = {
 circle.getDiameter(); // 10
 const a = (cb) => cb(); //다른 함수로 callback으로 받은경우
 a(circle.getDiameter); // NaN // 함수로 넘긴것, 메서드로 넘긴게 아니라, 다른 함수로 한번 더 감싼경우여서
+
+var add1 = new Function('x', 'y', 'return x + y');
+var add2 = function (x, y) {
+    return x + y;
+};
+console.log(add(2, 5));
+//위 두개는 동일하게 나온다.
+// function 생성자를 생성한 함수는 closer를 생성하지 않는등 함수 선언문이나 함수 표현식으로 생성한 함수는 다르게 동작한다.
+
+const add1 = function (x, y) {
+    return x + y;
+};
+const add2 = (x, y) => x + y;
+//화살표 함수 => 프로토타입이 없어서 생성자 함수 사용 불가능하다.
